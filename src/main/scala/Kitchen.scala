2da29a39c015c4eed3b39a2b0a8f86fc0e40d695
Jolanta Pockaja <jolantapockaja@gmail.com>
2020-09-03 21:52:55 +0300

    Kitchen recipes with zip

--- src/main/scala/Kitchen.scala
+++ src/main/scala/Kitchen.scala
@@ -1,4 +1,12 @@
 object Kitchen extends App {
 println("Starting cooking!")
-  val salad = new Recipe("Summer Salad", Seq("tomatoes", "sour cream"), Seq("cut into pieces", "mix"), 5)
+  val salad = new Recipe("Summer Salad", Seq("tomatoes", "sour cream","onions","paprika"), Seq("cut into pieces", "mix","chop"), 5)
+  salad.printIngredients()
+  salad.tupIngredients.foreach(println)
+// jobs = salad.cook(salad.tupIngredients,Seq("cut into pieces","mix","chop"))
+
+val jobs= salad.superCook()
+  println(jobs)
+  val soup = new Recipe("borsch", Seq("beets","onions","beefstock","carrot"), Seq("cut","chop","split","boil","extra rinse"),90)
+  println(soup.superCook())
 }
--- src/main/scala/Recipe.scala
+++ src/main/scala/Recipe.scala
@@ -1,8 +1,27 @@
+
+
 class Recipe (name: String, ingredients:Seq[String],instructions: Seq[String], cookTime:Int) extends Cooking {
 //val macCheese = cook("cheese","shred")
+def printIngredients() =
+for (item <- ingredients){
+  println(item)
+}
+def getTupSeq(): Seq[(String, String)] = {
+  var mySeq = ingredients.slice(0,ingredients.size -1) zip ingredients.slice(1,ingredients.size)
 
-  override def cook(ingredients: Seq[(String, String)], instructions: Seq[String]): String = {
-
-    "Big mess"
+  mySeq
+}
+def superCook():String = {
+  val tasks = cook(tupIngredients,instructions)
+  s"Recipe for:\n$name\n\n$tasks\n\nTakes $cookTime minutes to make."
+  // we use pur internal instructions
+}
+  override def cook(tupIngredients: Seq[(String, String)], instructions: Seq[String]): String = {
+val todos = tupIngredients zip instructions
+   // todos.foreach(todoss => println(s"Take ${todoss._1._1} then ${todoss._2} with ${todoss._1._2}"))
+    val textInstructions = todos.map(todo => s"Take ${todo._1._1} then ${todo._2} with ${todo._1._2}")
+   // "Big mess"
+    textInstructions.mkString("\n")
   }
+  val tupIngredients = getTupSeq()
 }
